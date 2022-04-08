4d27480bdf56e9dcaddd210fd0b8f62beb20e116
Will <69020009+snedex@users.noreply.github.com>
2022-04-07 21:46:35 -0400

    Add migrations and altered id generation

--- src/DesApi/Domain/DiffEntry.cs
+++ src/DesApi/Domain/DiffEntry.cs
@@ -9,7 +9,9 @@ namespace DesApi.Domain
     [Table("DiffEntry")]
     public class DiffEntry
     {
+        //We will create entries as specified by the user for now.
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
         public string Left { get; set; }
---
+++ src/DesApi/Migrations/20220408014408_Removed Auto Identity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesApi.Migrations
+{
+    public partial class RemovedAutoIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "DiffEntry",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "DiffEntry",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
